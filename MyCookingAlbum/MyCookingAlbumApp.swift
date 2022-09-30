8705c162943e67d7efa9ee0c255261ee01f6b89f
rionagasaki <naga_ri@icloud.com>
2022-10-01 08:27:23 +0900

    Initial Commit

---
+++ MyCookingAlbum/MyCookingAlbumApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyCookingAlbumApp.swift
+//  MyCookingAlbum
+//
+//  Created by Rio Nagasaki on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct MyCookingAlbumApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
